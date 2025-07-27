9d13a2292f3597dd703e9bdce1707c3accb680f0
And Fisher <and.fisher@hotmail.com>
2025-07-27 17:03:36 +0100

    Update content.js - Addition for obnoxious overlay
    Overlay didn't provide convenient "opt-out all" capability, so bye it goes.

--- content.js
+++ content.js
@@ -1,13 +1,18 @@
 function theTool() {
+    console.info('Load the tool',  document.querySelectorAll('[id^="sp_message_container"], [class^="overlay_"]'));
     document.querySelectorAll('html, body').forEach(el => {
         el.classList.remove('sp-message-open');
         el.classList.add('allow-scroll');
         el.style.overflow = 'auto !important';
     });
 
-    document.querySelectorAll('[id^="sp_message_container"], [class^="overlay_"]').forEach(modal => {
-        modal.style.opacity = '0 !important';
-        modal.style.pointerEvents = 'none !important';
+    document.querySelectorAll(`
+        [id^="sp_message_container"],
+        [class^="overlay_"],
+        #onetrust-consent-sdk`
+    ).forEach(modal => {
+        modal.style.opacity = 0;
+        modal.style.pointerEvents = 'none';
     });
 
     document.querySelectorAll('[class^="overlay_"]').forEach(el => {
